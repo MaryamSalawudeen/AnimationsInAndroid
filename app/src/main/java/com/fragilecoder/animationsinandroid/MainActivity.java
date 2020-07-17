32c8073133597522d0514b50a07a3f7359f698cf
thatfragilemuslimah <shecodeswell@gmail.com>
2020-07-17 17:10:45 +0100

    Mastering Animations in Android Second Commit

--- app/src/main/java/com/fragilecoder/animationsinandroid/MainActivity.java
+++ app/src/main/java/com/fragilecoder/animationsinandroid/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
     private TextView txt_helloAnimate;
 
     private boolean ishelloWorldIsShowing;
+
+    private TextView txtAndroid;
+
+    private Button btnAnimate;
 
 
     private ImageView imgLion, imgPeacock;
@@ -28,9 +33,16 @@ public class MainActivity extends AppCompatActivity {
 
         txt_helloWorld = findViewById(R.id.txt_helloWorld);
         txt_helloAnimate = findViewById(R.id.txt_helloAnimate);
+        txtAndroid = findViewById(R.id.txtAndroid);
 
         imgLion = findViewById(R.id.imgLion);
         imgPeacock = findViewById(R.id.imgPeacock);
+
+        btnAnimate = findViewById(R.id.btnAnimate);
+
+        txt_helloWorld.setX(2000);
+        txt_helloAnimate.setX(-2000);
+        txtAndroid.setY(-3000);
 
         txt_helloWorld.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +52,9 @@ public class MainActivity extends AppCompatActivity {
 
 //                txt_helloWorld.animate().alpha(0).setDuration(3000);
 //                txt_helloAnimate.animate().alpha(1).setDuration(3000);
+
+
+
                 if (ishelloWorldIsShowing == true) {
                     txt_helloWorld.animate().alpha(0).setDuration(3000);
                     txt_helloAnimate.animate().alpha(1).setDuration(3000);
@@ -59,18 +74,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if (isAnimalShowing == true) {
-                    imgLion.animate().alpha(0).setDuration(3000);
-                    imgPeacock.animate().alpha(1).setDuration(3000);
+                    imgLion.animate().alpha(0).translationX(300).rotationY(2000).setDuration(3000);
+                    imgPeacock.animate().alpha(1).translationX(-300).rotationY(2000).setDuration(3000);
                     isAnimalShowing = false;
                 } else if (!isAnimalShowing) {
-                    imgLion.animate().alpha(1).setDuration(3000);
-                    imgPeacock.animate().alpha(0).setDuration(3000);
+                    imgLion.animate().alpha(1).translationX(300).rotationY(2000).setDuration(3000);
+                    imgPeacock.animate().alpha(0).translationX(-300).rotationY(2000).setDuration(3000);
                     isAnimalShowing = true;
                 }
             }
         });
 
+        txtAndroid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                txtAndroid.animate().translationX(3000f).setDuration(2000);
+//                txtAndroid.animate().translationY(3000).setDuration(3000);
 
+//                txtAndroid.animate().translationXBy(200).setDuration(5000);
+//
+//                txtAndroid.animate().rotation(360).setDuration(5000);
+
+
+//                txtAndroid.animate().rotationBy(360).setDuration(4000);
+
+                txtAndroid.animate().rotationX(400).translationY(2000).setDuration(3000);
+            }
+        });
+
+        btnAnimate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt_helloWorld.animate().translationXBy(-2000).rotation(2000).setDuration(3000);
+                txt_helloAnimate.animate().translationXBy(2000).scaleX(200).setDuration(2000);
+                txtAndroid.animate().translationYBy(3000).alpha(0.7f).setDuration(4000);
+            }
+        });
 
     }
 }
